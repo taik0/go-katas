d2afa945b80496263197633e94535e3a782de170
taik0 <suikogaiden@gmail.com>
2017-12-20 00:00:57 +0100

    Kpacha Ka€ta 02: Flaky backend. Step 1. Add the random string generator

--- flaky-backend.go
+++ flaky-backend.go
@@ -2,12 +2,26 @@ package main
 
 import "flag"
 import "fmt"
+import "math/rand"
+import "time"
+
 
 func main() {
 
-  var port int
-  flag.IntVar(&port, "port", 8081, "Listen port for the flaky backend.")
-  flag.Parse()
+  rand.Seed(time.Now().Unix())
+  var sku_len int = 6
+	port := flag.Int("port", 8081, "Listen port for the flaky backend.")
+	flag.Parse()
 
-  fmt.Printf("Port flag value: %d\n", port)
+	fmt.Printf("Port flag value: %d\n", *port)
+	x := randStrGen(sku_len)
+	fmt.Println(x)
+}
+
+func randStrGen(len int) string {
+	random_array := make([]rune, len)
+	for i, v := range rand.Perm(26)[:len] {
+		random_array[i] = rune(v) + 65
+	}
+	return string(random_array)
 }
