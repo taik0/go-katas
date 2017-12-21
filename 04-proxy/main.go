e3cf3a66e84e97263c559ab8673f905aba0e4af5
taik0 <suikogaiden@gmail.com>
2017-12-22 00:20:43 +0100

    remove delays from the parse function. Add port flag. go fmt

--- 04-proxy/main.go
+++ 04-proxy/main.go
@@ -4,11 +4,11 @@ import (
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"time"
-  "github.com/gin-gonic/gin"
 )
 
 type Stock struct {
@@ -22,36 +22,40 @@ type Stock struct {
 func main() {
 
 	rand.Seed(time.Now().Unix())
+	port := flag.Int("port", 8081, "Listen port for the flaky backend.")
+	flag.Parse()
+
+	fmt.Printf("Port flag value: %d\n", *port)
 
 	done := make(chan []byte)
 
 	r := gin.Default()
 	r.GET("/product", func(c *gin.Context) {
-    resp, err := http.Get("http://localhost:8081/product")
-  	if nil != err {
-      c.AbortWithStatus(500)
-  	}
+		resp, err := http.Get("http://localhost:8081/product")
+		if nil != err {
+			c.AbortWithStatus(500)
+		}
 
-  	defer resp.Body.Close()
-  	if resp.StatusCode == 500 {
-  		c.AbortWithStatus(500)
-  	}
+		defer resp.Body.Close()
+		if resp.StatusCode == 500 {
+			c.AbortWithStatus(500)
+		}
 
-  	products, err := ioutil.ReadAll(resp.Body)
+		products, err := ioutil.ReadAll(resp.Body)
 
-  	if nil != err {
-  		fmt.Println("Error reading data: ", err.Error())
-  		c.AbortWithStatus(500)
-  	}
+		if nil != err {
+			fmt.Println("Error reading data: ", err.Error())
+			c.AbortWithStatus(500)
+		}
 
-  	for i := 0; i < 10; i++ {
-  		go Parse(products, done)
-  	}
+		for i := 0; i < 10; i++ {
+			go Parse(products, done)
+		}
 
-  	data := <-done
+		data := <-done
 		c.String(200, string(data))
 	})
-	r.Run()
+	r.Run(fmt.Sprintf(":%d", *port))
 
 }
 
@@ -59,20 +63,17 @@ func Parse(products []byte, done chan<- []byte) {
 
 	stock := Stock{}
 
-	time.Sleep(time.Duration(rand.Int31n(1000)) * time.Millisecond)
 	err := xml.Unmarshal(products, &stock)
 	if err != nil {
 		fmt.Println("Error Unmarshaling XML:", err.Error())
 		return
 	}
 
-	time.Sleep(time.Duration(rand.Int31n(1000)) * time.Millisecond)
 	data, err := json.Marshal(stock)
 	if nil != err {
 		fmt.Println("Error Marshaling to JSON:", err.Error())
 		return
 	}
 
-	time.Sleep(time.Duration(rand.Int31n(1000)) * time.Millisecond)
 	done <- data
 }
